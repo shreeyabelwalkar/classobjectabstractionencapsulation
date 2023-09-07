3359f276cb009dff622d9eaab6bfc51489b4042f
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-09-07 11:39:29 +0530

    New file inheritancedemo.py is created

---
+++ inheritancedemo.py
@@ -0,0 +1,24 @@
+class Employee:
+    def setid(self,id):
+        self.id=id
+
+    def getid(self):
+        return self.id
+
+    def setName(self,name):
+        self.name=name
+
+    def getName(self):
+        return self.name
+
+    def setAddress(self,address):
+        self.address=address
+
+    def getAddress(self):
+        return self.address
+
+    def setSalary(self,salary):
+        self.salary=salary
+
+    def getSalary(self):
+        return self.salary
